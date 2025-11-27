f6d2a0920be71f59a30490ff5437e805d6d74fde
chandu078 <chandudyavanapelli03@gmail.com>
2025-11-27 18:33:24 +0000

    sm8650-common: Include previously omitted changes
    Change-Id: I9e74a0f8df8d3cc63988aa040ca2a1dbb0c91ff6

--- extract-files.py
+++ extract-files.py
@@ -78,6 +78,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'-e "zram" -e "zsmalloc"', ''),
     'vendor/bin/vendor_modprobe.sh': blob_fixup()
         .regex_replace(r'\n.*OPLUS_BUG_STABILITY[\s\S]*?OPLUS_BUG_STABILITY.*\n', ''),
+    'vendor/etc/media_cliffs_v1/video_system_specs.json': blob_fixup()
+        .regex_replace(r'"dump_location"\s*:\s*0', '"dump_location": 0,\n            "default_primaries_override" : 1'),
     ('vendor/etc/media_codecs_cliffs_v1.xml', 'vendor/etc/media_codecs_pineapple.xml'): blob_fixup()
         .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
     'vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy': blob_fixup()
